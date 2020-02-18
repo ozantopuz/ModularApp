4251d7315bbed22eced68ab2e824bcd129d32236
ozan <ozantopuz8@gmail.com>
2020-02-18 13:20:22 +0300

    game modules added

---
+++ game/game_domain/build/generated/source/buildConfig/dev/debug/com/ozan/myapplication2/game/domain/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ozan.myapplication2.game.domain;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.ozan.myapplication2.game.domain";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.ozan.myapplication2.game.domain";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "dev";
+  public static final int VERSION_CODE = 10000;
+  public static final String VERSION_NAME = "1.0.0";
+}
